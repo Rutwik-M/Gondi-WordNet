be2cdd26df127daa2ca1a21828bd828c9ca5f11d
ruts558 <rmuley558@gmail.com>
2025-04-26 01:04:34 +0530

    config.example

---
+++ config.example.py
@@ -0,0 +1,10 @@
+# config.example.py
+
+import os
+
+class Config:
+    DATABASE_URL      = os.getenv("DATABASE_URL")
+    LOGIN_DATABASE_URL= os.getenv("LOGIN_DATABASE_URL")
+    SUPABASE_URL      = os.getenv("SUPABASE_URL")
+    SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+    SECRET_KEY        = os.getenv("SECRET_KEY")
