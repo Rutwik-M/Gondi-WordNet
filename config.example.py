695f5fb75cdfd4e82ae5e10507fb0e03137b5f2c
Ruts-Linux <ruts-linux@Ruts-Linux.Rut's-Linux>
2025-08-04 22:55:29 +0530

    main

--- config.example.py
+++ config.example.py
@@ -2,11 +2,12 @@
 
 import os
 
+ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+
 class Config:
     DATABASE_URL      = os.getenv("SUPABASE_DATABASE_URL")
     LOGIN_DATABASE_URL= os.getenv("LOGIN_DATABASE_URL")
     SUPABASE_URL      = os.getenv("SUPABASE_URL")
     SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
     SECRET_KEY        = os.getenv("SECRET_KEY")
-    ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
